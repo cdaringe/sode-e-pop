dedaf1ef32a08d0ec0815e23c563bd692672605b
cdaringe <cdaringe@gmail.com>
2023-11-07 19:26:26 -0800

    feat: add sprite

--- settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const setting = {
-    type: "int-setting",
-    name: "yoked-up-multiplier",
-    default_value: 2,
-    setting_type: "runtime-global",
-};
-data.extend([setting]);
